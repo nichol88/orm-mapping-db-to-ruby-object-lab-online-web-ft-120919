28a7b308c126837d039e27582bf8d666427efd9e
Brian Nicholls <nichol88@gmail.com>
2020-01-22 18:02:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,10 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def self.first_student_in_grade_10
+    
+  end
+
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10 LIMIT ?
